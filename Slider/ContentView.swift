8f0248ba35ff88016b87700c2e823169b2bd6a43
Edwin <edwin12sv@gmail.com>
2023-12-24 00:42:30 -0600

    Initial Commit

---
+++ Slider/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Slider
+//
+//  Created by elsalvador on 24/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Slider/SliderApp.swift
@@ -0,0 +1,17 @@
+//
+//  SliderApp.swift
+//  Slider
+//
+//  Created by elsalvador on 24/12/23.
+//
+
+import SwiftUI
+
+@main
+struct SliderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
